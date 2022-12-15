a23c757db73e0b502410a07edb08a7edc5fe27b9
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2022-12-15 17:24:50 +0530

    Update checker.hpp

--- checker.hpp
+++ checker.hpp
@@ -3,3 +3,4 @@
 bool batteryTempIsOk(float temperature, float UppTempLim, float LowTempLim);
 bool SocisOk(float soc, float UppSocLim, float LowSocLim);
 bool ChargeRateIsOk(float ChargeRate, float BattLim);
+void test_checker();
