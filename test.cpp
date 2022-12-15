c5f908c7bfbbcfec2b5287845651014f8ec11a92
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2022-12-15 17:16:31 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ assert(batteryTempIsOk(50, 45, 0) == false);
 assert(batteryTempIsOk(25, 45, 0) == true);
 
 assert(SocisOk(60, 80, 0) == true);
-assert(SocisOk(90, 80, 0) == false));    
+assert(SocisOk(90, 80, 0) == false);    
   
 assert(ChargeRateIsOk(20, 80) == true);
 assert(ChargeRateIsOk(90, 80) == false);
